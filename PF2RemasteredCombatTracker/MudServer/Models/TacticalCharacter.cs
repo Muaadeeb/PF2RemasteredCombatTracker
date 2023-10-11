dc73a823ceec4cb3c8dd7f255adccee3a31367f5
Timothy Ingledue <TimothyIngledue@gmail.com>
2023-10-11 14:19:57 -0500

    Characters Done, Weapons done,  Combat Tracker - inputs and selctions done.

---
+++ PF2RemasteredCombatTracker/MudServer/Models/TacticalCharacter.cs
@@ -0,0 +1,8 @@
+﻿namespace MudServer.Models;
+public class TacticalCharacter
+{
+    public CharacterCodex OriginalCharacter { get; set; }
+    public bool IsFoe { get; set; }
+    public int Initiative { get; set; }
+}
+
